a1bd5254c58f25bfbda8a3c06f7ad2c1b627327d
max <maxj07011@gmail.com>
2026-09-17 23:50:41 +0000

    worked till part 4

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,17 +1,25 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the manifest file in the resource directory.
- */
 public class Robot extends TimedRobot {
+
+    XboxController controller = new XboxController(0);
+
+    SparkMax leftmotor1 = new SparkMax(0, MotorType.kBrushless);
+    SparkMax leftmotor2 = new SparkMax(0, MotorType.kBrushless);
+    SparkMax rightmotor1 = new SparkMax(0, MotorType.kBrushless);
+    SparkMax rightmotor2 = new SparkMax(0, MotorType.kBrushless);
+
+    Constaints.DriveConstraints.kDriveControllerPort;
+
+    DifferentialDrive Drive = new DifferentialDrive(leftmotor1,rightmotor1);
+
+
     /** Called once at the beginning of the robot program. */
     public Robot() {
 
@@ -19,7 +27,9 @@ public class Robot extends TimedRobot {
 
     /** This function is run once each time the robot enters autonomous mode. */
     @Override
-    public void autonomousInit() {}
+    public void autonomousInit() {
+
+    }
 
     /** This function is called periodically during autonomous. */
     @Override
@@ -29,17 +39,25 @@ public class Robot extends TimedRobot {
 
     /** This function is called once each time the robot enters teleoperated mode. */
     @Override
-    public void teleopInit() {}
+    public void teleopInit() {
+
+    }
 
     /** This function is called periodically during teleoperated mode. */
     @Override
-    public void teleopPeriodic() {}
+    public void teleopPeriodic() {
+
+    }
 
     /** This function is called once each time the robot enters test mode. */
     @Override
-    public void testInit() {}
+    public void testInit() {
+
+    }
 
     /** This function is called periodically during test mode. */
     @Override
-    public void testPeriodic() {}
+    public void testPeriodic() {
+        
+    }
 }
---
+++ src/main/java/frc/robot/differentialdrive.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public final class differentialdrive {
+    public static final int joy_Port = 0;
+    public static final int l_Motor = 4;
+    public static final int l_Motor2 = 5;
+    public static final int r_Motor = 6;
+    public static final int r_Motor2 = 7;
+
+}
